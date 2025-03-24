a00cf0aa555f963663a27cdb118e6f10bc93e284
hariachmad <hari08achmad@gmail.com>
2025-03-24 08:59:27 +0700

    ADD LOGGER TO PROMETHEUS MIDDLEWARE

--- dockerfile
+++ dockerfile
@@ -1,27 +1,15 @@
 FROM node:20 AS builder
 
-# Create app directory
 WORKDIR /app
 
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
 COPY package*.json ./
 
-# Install app dependencies
 RUN npm install
 
 COPY . .
 
-COPY prisma ./prisma/
-
 RUN npx prisma generate
 RUN npm run build
-
-FROM node:20
-
-COPY --from=builder /app/node_modules ./node_modules
-COPY --from=builder /app/prisma ./prisma
-COPY --from=builder /app/package*.json ./
-COPY --from=builder /app/dist ./dist
 
 EXPOSE 3000
 CMD [ "npm", "run", "start:prod" ]
--- src/prometheus/prometheus.middleware.ts
+++ src/prometheus/prometheus.middleware.ts
@@ -1,17 +1,21 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { PrometheusService } from './prometheus.service';
+import { GlobalLogger } from 'src/global-logger/global-logger.service';
 
 @Injectable()
 export class PrometheusMiddleware implements NestMiddleware {
-  constructor(private readonly prometheusService: PrometheusService) {}
+  constructor(
+    private readonly prometheusService: PrometheusService,
+    private readonly logger: GlobalLogger,
+  ) {}
   use(req: any, res: any, next: () => void) {
     const { method, originalUrl } = req;
-    const imei = req.body['npk'];
-    console.log('imei: ', imei);
+    const npk = req.body['npk'];
+    this.logger.log('npk:' + npk + 'sending Penerimaan Getah');
     this.prometheusService.incrementRequestCounter(
       method,
       originalUrl,
-      imei,
+      npk,
       '200',
     );
     next();
